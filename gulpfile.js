a6e2a24e4702e0f50c36ed3bd14e296295b3b0ea
Kevin Hu <hu.kevin.w@gmail.com>
2018-09-19 10:28:01 -0400

    Ugly-fy html

--- gulpfile.js
+++ gulpfile.js
@@ -39,7 +39,7 @@ gulp.task('build', ['compileSass'], function() {
       .pipe(replace(new RegExp('/sass/(.+).scss', 'ig'), '/css/$1.css'))
 
       // compile using Pug
-      .pipe(pug({ doctype: 'html', pretty: true }))
+      .pipe(pug({ doctype: 'html', pretty: false }))
 
       // inline CSS
       .pipe(inlineCss({ preserveMediaQueries: true }))
